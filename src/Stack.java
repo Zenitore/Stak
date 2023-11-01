54630f6ff14ed159a64200f35b5be6f316d4bdd5
Zenitore <eayers25@kentdenver.org>
2023-10-31 22:10:44 -0600

    creation of push branch and addition of push method

--- src/Stack.java
+++ src/Stack.java
@@ -1,5 +1,18 @@
 public class Stack<T> {
 
+    StackNode<T> currentNode = new StackNode<>();
+    public Stack(){
+    }
+
+    public void push(T element){
+        if (currentNode.getValue() != null){
+            StackNode<T> createdNode = new StackNode<>(element, currentNode);
+            currentNode = createdNode;
+        }
+        else{
+            currentNode.setValue(element);
+        }
+    }    //add an element
 
 
 }
